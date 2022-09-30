05aed9da1a59097c9afbc9d165b905e1264a33bd
Dominic Battre <battre@google.com>
2022-09-30 20:41:14 +0200

    New approach of analyzing structured addresses

---
+++ structured_analysis/src/extract_fields.py
@@ -0,0 +1,139 @@
+# This is a tool to extract form structures from an HTML file.
+#
+# The HTML file should be a dump of a website from Chrome with the
+# chrome://flags/#show-autofill-type-predictions flag enabled to extract
+# metadata about Chrome Autofill's interpretation of the form.
+#
+# You have to pass the language and country of the website:
+# --country=EG   # for Egypt
+# --language=ar  # for Arabic
+# file.html      # the file from which we extract form controls
+#
+# If you pass --label-follows-field, the field labels extracted by Chrome
+# are ignored and we take the text following form controls. For some websites
+# this works better than Chrome's current label detection.
+#
+# If you pass --translate, all labels are translated via Google Translate.
+
+import address_pb2
+import argparse
+from bs4 import BeautifulSoup
+from google.protobuf import text_format
+from google.cloud import translate_v2 as translate
+
+
+# If Chrome is started with chrome://flags/#show-autofill-type-predictions
+# enabled, an autofill-information attribute of form controls contains
+# information about Chrome autofill's interpretation of the form control.
+def extract_autofill_information(element):
+  if not element.has_attr("autofill-information"):
+    return ""
+  autofill_information = [
+      row.strip() for row in element["autofill-information"].split("\n")
+  ]
+  autofill_information = [row.split(": ", 2) for row in autofill_information]
+  autofill_information = [row for row in autofill_information if len(row) == 2]
+  return {row[0]: row[1] for row in autofill_information}
+
+
+# For some fields Chrome does not extract the label correctly today. This is
+# an alternative strategy to discover labels by looking at the text after
+# a form control. You can enable this strategy with the --label-follows-field
+# command line parameter.
+def extract_label_after_field(element):
+  element = element.next_sibling
+  if not element:
+    return ""
+  if element.string and element.string.strip():
+    return element.string.strip()
+  return ""
+
+
+parser = argparse.ArgumentParser(description="Extract fields of forms.")
+parser.add_argument(
+    "--country",
+    type=str,
+    metavar="cc",
+    help="two-letter country code (eg. 'EN') as per ISO 3166-1",
+    required=True,
+)
+parser.add_argument(
+    "--language",
+    type=str,
+    metavar="ll",
+    help="two-letter language code (e.g. 'en') as per ISO 639-1.",
+    required=True,
+)
+parser.add_argument(
+    "--label-follows-field",
+    action="store_true",
+    help="label follows field (in some languages or sites, the label of a field"
+    " follow the field. In this case, chrome does a poor job at extracting"
+    " the label.",
+)
+parser.add_argument(
+    "--translate", action="store_true", help="enable Google translate")
+parser.add_argument(
+    "file",
+    type=str,
+    metavar="file",
+    help="HTML file from which to extract fields.",
+)
+
+args = parser.parse_args()
+
+f = open(args.file, "rb")
+soup = BeautifulSoup(f.read(), "html.parser")
+f.close()
+
+ontology = address_pb2.AddressOntology()
+site_example = ontology.site_examples.add()
+site_example.locale.country = args.country
+site_example.locale.language = args.language
+site_example.url = "todo"
+site_example.is_structured = False
+
+translate_client = translate.Client()
+
+sequence = site_example.sequences.add()
+sequence.section = address_pb2.ExampleSequenceSection.UNDEFINED
+sequence.hide = False
+for html_field in soup.find_all(["input", "select"]):
+  field = sequence.fields.add()
+  field.name = ((html_field["id"] if html_field.has_attr("id") else "") + "|" +
+                (html_field["name"] if html_field.has_attr("name") else ""))
+  placeholder = (
+      html_field["placeholder"] if html_field.has_attr("placeholder") else "")
+  autocomplete = (
+      html_field["autocomplete"] if html_field.has_attr("autocomplete") else "")
+
+  autofill_information = extract_autofill_information(html_field)
+
+  if args.label_follows_field:
+    field.label = extract_label_after_field(html_field)
+  else:
+    field.label = autofill_information.get("label", "")
+  if field.label and args.translate:
+    translation = translate_client.translate(
+        field.label, target_language="en", source_language=args.language)
+    field.label_translated = translation["translatedText"]
+  if placeholder:
+    field.example = placeholder
+    if args.translate:
+      translation = translate_client.translate(
+          placeholder, target_language="en", source_language=args.language)
+      field.example_translated = translation["translatedText"]
+
+  field.control_type = address_pb2.ControlType.UNSPECIFIED
+  if html_field.name == "input":
+    field.control_type = address_pb2.ControlType.INPUT
+  elif html_field.name == "select":
+    field.control_type = address_pb2.ControlType.SELECT
+
+  if autocomplete:
+    field.autocomplete_attribute = autocomplete
+
+  field.concepts.append("unset")
+  field.section = address_pb2.ExampleSequenceSection.ADDRESS
+
+print(text_format.MessageToString(ontology, as_utf8=True))
---
+++ structured_analysis/src/generate_html.py
@@ -0,0 +1,137 @@
+# This tool takes a couple of .textproto files containig a AddressOntology
+# proto and assembles a human readable representation via "template.html"
+# into the file "out.html"
+
+import address_pb2
+import argparse
+from typing import Generator
+from google.protobuf import text_format
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import os
+import sys
+
+parser = argparse.ArgumentParser(
+    description="Generates the summary of information from the provided "
+    "textprotos.")
+parser.add_argument(
+    "--output",
+    type=str,
+    metavar="output",
+    help="HTML file to generate",
+    required=True,
+)
+parser.add_argument(
+    'input',
+    metavar='inputs',
+    type=str,
+    nargs='+',
+    help='Textproto files containing a AddressOntology object')
+
+args = parser.parse_args()
+
+# Uses the address_pb2.ExampleFieldSequence.fields.section attribute to
+# gather all fields with a common value in one address_pb2.ExampleFieldSequence.
+def split_sequence(sequence: address_pb2.ExampleFieldSequence
+                  ) -> Generator[address_pb2.ExampleFieldSequence, None, None]:
+  # Build a list of the different unique values of
+  # address_pb2.ExampleFieldSequence.fields.section
+  # sorted by order of occurrence.
+  sections_found = []
+  for field in sequence.fields:
+    if field.section not in sections_found:
+      sections_found.append(field.section)
+
+  # Create a new address_pb2.ExampleFieldSequence for each value of the section.
+  for section in sections_found:
+    new_sequence = address_pb2.ExampleFieldSequence()
+    new_sequence.section = section
+    for field in sequence.fields:
+      if field.section == section:
+        new_sequence.fields.append(field)
+    yield new_sequence
+
+
+# Takes each address_pb2.ExampleFieldSequence of site_example and splits it
+# into multiple ExampleFieldSequences of fields which share a common section.
+def split_sequences(site_example: address_pb2.SiteExample) -> None:
+  old_site_example = address_pb2.SiteExample()
+  old_site_example.CopyFrom(site_example)
+  del site_example.sequences[:]
+  for sequence in old_site_example.sequences:
+    for new_sequence in split_sequence(sequence):
+      site_example.sequences.append(new_sequence)
+
+
+# Populates an "internal_id" with values that are unique for each field.
+# This allows us to reference the values in the HTML code.
+def assign_ids_to_fields(ontology: address_pb2.AddressOntology) -> None:
+  counter = 0
+  for site in ontology.site_examples:
+    for sequence in site.sequences:
+      for field in sequence.fields:
+        field.internal_id = "field_{}".format(counter)
+        counter = counter + 1
+
+
+def remove_if_hidden(container) -> None:
+  items_to_remove = [item for item in container if item.hide]
+  for item in items_to_remove:
+    container.remove(item)
+
+
+# The main program.
+
+if len(sys.argv) == 1:
+  print("Usage:", sys.argv[0], "files.textproto")
+  sys.exit(-1)
+
+# Merge all ontology files into one big data structure.
+ontology = address_pb2.AddressOntology()
+for file in args.input: # sys.argv[1:]:
+  # Read and parse ontology file.
+  f = open(file, "rb")
+  ontology_in_file = address_pb2.AddressOntology()
+  text_format.Parse(f.read(), ontology_in_file)
+  f.close()
+  ontology.MergeFrom(ontology_in_file)
+
+# Remove everything that is hidden.
+remove_if_hidden(ontology.site_examples)
+for site_example in ontology.site_examples:
+  remove_if_hidden(site_example.sequences)
+  for sequence in site_example.sequences:
+    remove_if_hidden(sequence.fields)
+
+# Split sequences of fields by type [address, phone, address] becomes
+# [address, address], [phone].
+for site_example in ontology.site_examples:
+  split_sequences(site_example)
+
+# Give every field a unique id.
+assign_ids_to_fields(ontology)
+
+# Determine all known concepts (e.g. first name, address-level1, ...) in the
+# ontology.
+known_concepts = set()
+for concept in ontology.concepts:
+  known_concepts.add(concept.name)
+for concept in ontology.compound_concepts:
+  known_concepts.add(concept.name)
+
+# Determine all countries for which we have examples.
+countries = set([example.locale.country for example in ontology.site_examples])
+
+# Render everything to out.html.
+env = Environment(
+    loader=FileSystemLoader(os.path.dirname(__file__)),
+    autoescape=select_autoescape())
+template = env.get_template("template.html")
+result = template.render(
+    ontology=ontology,
+    countries=countries,
+    address_pb2=address_pb2,
+    rtl_languages=set(["ar"]),
+    known_concepts=known_concepts)
+f = open(args.output, "w")
+f.write(result)
+f.close()
