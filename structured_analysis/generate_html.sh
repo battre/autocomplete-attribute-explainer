8207ff9abc7c2e52df7323e344d2c40560beddf1
Dominic Battre <battre@google.com>
2022-09-30 20:43:19 +0200

    Move output to host it as a github website

--- structured_analysis/generate_html.sh
+++ structured_analysis/generate_html.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 src/generate_html.py  --output ../www/index.html data/*.textproto data/sites/*.textproto data/sites/*/*.textproto
+python3 src/generate_html.py  --output ../docs/index.html data/*.textproto data/sites/*.textproto data/sites/*/*.textproto
